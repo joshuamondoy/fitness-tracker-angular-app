812ceab2e4a787a84533fc9eec8ec65075114ccb
Mondoy, Joshua <joshua.mondoy@accenture.com>
2022-02-02 15:23:21 +0800

    remove exerciseName

--- src/app/training/current-training/completed-training.component.ts
+++ src/app/training/current-training/completed-training.component.ts
@@ -1,13 +1,17 @@
 import { Component, Inject, OnInit} from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { TrainingService } from '../training.sevice';
+import { Store } from '@ngrx/store';
+import * as fromTraining from '../training.reducer';
+import * as fromRoot from '../../app.reducer';
+import { take } from 'rxjs/operators';
 
 
 @Component({
   selector: 'app-stop-training',
   template: `<div fxLayoutAlign="center center" fxLayout="column">
                 <h1 mat-dialog-title>Congratulations!</h1>
-                <mat-dialog-content>You already completed {{ exerciseName }}.</mat-dialog-content>
+                <mat-dialog-content>You already completed.</mat-dialog-content>
                 <mat-dialog-actions>
                     <button mat-flat-button color="accent" [mat-dialog-close]="true">Close</button>
                 </mat-dialog-actions>
@@ -17,10 +21,13 @@ export class CompletedTrainingComponent implements OnInit {
   exerciseName: string;
 
   constructor(@Inject(MAT_DIALOG_DATA) public passedData: any,
-              private trainingService: TrainingService) { } // this is like subscribing to a subject
+              private trainingService: TrainingService,
+              private store: Store<fromTraining.State>) { } // this is like subscribing to a subject
 
   ngOnInit(): void {
-      this.exerciseName = this.trainingService.exercisesName
+      // this.exerciseName = this.trainingService.exercisesName
+     
+      
   }
   
 
